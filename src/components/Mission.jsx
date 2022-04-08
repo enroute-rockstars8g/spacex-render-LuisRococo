961c53b5ab7455d4e63b655132854daa2ee1c34b
LuisRococo <luisrobertopenav@gmail.com>
2022-04-08 11:06:55 -0700

    Se corrigio bug, donde en el campo id aparecia name

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 import missionImg from "../img/mission-png-3.png";
 
-export default function Mission({title, id, description}) {
+export default function Mission({ title, id, description }) {
   return (
-    <div className='mission'>
-        <p className='mission__title'>{title}</p>
-        <hr className='sep' />
-        <div className='mission__cont'>
-            <div className='mission__cont-img'>
-                <img src={missionImg} alt="mission-img" />
-            </div>
-            <div className='mission__cont-info'>
-                <p className='mission__label'>Id</p>
-                <p className='mission__text'>{title}</p>
-                <p className='mission__label'>Description</p>
-                <p className='mission__text'>{description}</p>
-            </div>
+    <div className="mission">
+      <p className="mission__title">{title}</p>
+      <hr className="sep" />
+      <div className="mission__cont">
+        <div className="mission__cont-img">
+          <img src={missionImg} alt="mission-img" />
         </div>
+        <div className="mission__cont-info">
+          <p className="mission__label">Id</p>
+          <p className="mission__text">{id}</p>
+          <p className="mission__label">Description</p>
+          <p className="mission__text">{description}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
